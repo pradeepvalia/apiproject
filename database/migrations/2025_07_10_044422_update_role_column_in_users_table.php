89390a5dfbf1893f885d4595b155d36fd7ce4fc6
Pradeep Valiya <pradeep.valiya@teamlease.com>
2025-07-10 10:18:11 +0530

    Remove role column comment; allowed roles will be managed by API, not schema

--- database/migrations/2025_07_10_044422_update_role_column_in_users_table.php
+++ database/migrations/2025_07_10_044422_update_role_column_in_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user')->comment('admin, user, moderator, editor, viewer')->change();
+            $table->string('role')->default('user')->comment(null)->change();
         });
     }
 
